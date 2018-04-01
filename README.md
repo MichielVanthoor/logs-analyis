dc9bc93a3da8b94766af1dbbfab39393e8d058be
Michiel Vanthoor <michielvanthoor@gmail.com>
2018-04-01 11:12:15 +0200

    2nd Submission

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # Logs Analysis
 
+This Python script uses psycopg2 to query a mock PostgreSQL database for a fictional news website. 
+This script answers following questions using PostgreSQL queries:
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of requests lead to errors?
+
+
 Requirements:
-* Python 2.7
+* Python 3, PostgreSQL, psycopg2
 
 Steps to launch the analysis:
-*  Install the VM with the necessay dependencies and import the database as outlined in the Fullstack Developer Udacity course
-*  Clone this repo to your environment
-*  Navigate to the 'logs_analyis' folder in the 'projects' directory
-*  Copy **logs_analysis.py** to your vagrant folder, this way the VM has access to it
-*  Inside your VM, run **logs_analysis.py** (eg. `python logs_analysis.py`)
+* Download and unzip [newsdta.zip](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip), this file contains the database of the fictional news website
+* Import the schema and data from the database in your PostgreSQL environment
+* Run **logs_analysis.py** to launch the analysis(eg. `python logs_analysis.py`)
 
 Enjoy!
--- log_exercise.py
+++ log_exercise.py
@@ -1,10 +1,12 @@
-# Log Analysis Project
+#!/usr/bin/env python3
 
 import psycopg2
 from datetime import datetime
 
 
 def execute_sql(query):
+    """Connect to the news database, execute query and return results."""
+
     db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
     cursor.execute(query)
@@ -14,11 +16,13 @@ def execute_sql(query):
     return results
 
 
-def question1():
+def query_and_print_q1():
+    """Execute the query for question 1 and print the results."""
+
     print("1. What are the most popular three articles of all time?")
     results = execute_sql("SELECT articles.title, COUNT(log.id) as views \
             FROM log, articles \
-            WHERE log.path LIKE '%' || articles.slug \
+            WHERE log.path = '/article/' || articles.slug \
             AND log.status = '200 OK' AND log.method = 'GET' \
             GROUP BY articles.title \
             ORDER BY views DESC \
@@ -31,11 +35,13 @@ def question1():
     print
 
 
-def question2():
+def query_and_print_q2():
+    """Execute the query for question 2 and print the results."""
+
     print("2. Who are the most popular article authors of all time?")
     results = execute_sql("SELECT authors.name, COUNT(log.id) as views \
             FROM log,articles,authors \
-            WHERE log.path LIKE '%' || articles.slug \
+            WHERE log.path = '/article/' || articles.slug \
             AND articles.author = authors.id \
             GROUP BY authors.name \
             ORDER BY views DESC;  \
@@ -46,7 +52,8 @@ def question2():
     print
 
 
-def question3():
+def query_and_print_q3():
+    """Execute the query for question 3 and print the results."""
     print("3. On which days did more than 1% of requests lead to errors?")
     results = execute_sql("SELECT date, errors*100.0/total as rate \
             FROM ( \
@@ -59,12 +66,19 @@ def question3():
             ")
 
     for row in results:
-        print("{} - {}% errors".format(row[0].strftime("%b %d, %Y"),
-              round(row[1], 2)))
+        print('{0:%B %d, %Y} - {1:.2f}% errors'.format(row[0], row[1]))
 
-print("#####")
-print("Welcome to our Log Analysis!")
-print("#####")
-question1()
-question2()
-question3()
+    print
+
+
+def main():
+    print("#####")
+    print("Welcome to our Log Analysis!")
+    print("#####")
+    query_and_print_q1()
+    query_and_print_q2()
+    query_and_print_q3()
+
+
+if __name__ == '__main__':
+    main()
